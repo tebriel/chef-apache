271fbb952fef10b314e5ce7ce082e592e5788799
Chris Moultrie <Chris@Moultrie.org>
2015-10-20 14:45:08 +0000

    Now a template

--- recipes/server.rb
+++ recipes/server.rb
@@ -4,14 +4,4 @@ service 'httpd' do
   action [:enable, :start]
 end
 
-file '/var/www/html/index.html' do
-  content <<-EOH
-    <HTML>
-    <BODY>
-      <H1>Hello, world!</H1>
-      <H3>My IP Address is: #{node['ipaddress']}</H3>
-      <H3>My Hotname is: #{node['hostname']}</H3>
-    </BODY>
-    </HTML>
-  EOH
-end
+template '/var/www/html/index.html'
